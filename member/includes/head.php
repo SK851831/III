9d068a714355af70fdb67cab3743765903f1657d
#hacker <sk851831@gmail.com>
2015-03-24 21:33:20 +0530

    Font awesome updated

--- member/includes/head.php
+++ member/includes/head.php
@@ -3,7 +3,7 @@
         <title><?php  echo $system->title; ?>  &middot; Member &middot; Newsfeed</title>
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
         <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <link href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <link href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
         <!-- Ionicons -->
         <link href="//code.ionicframework.com/ionicons/1.5.2/css/ionicons.min.css" rel="stylesheet" type="text/css" />
         <!-- Morris chart -->
