6c2de2d349eccb1ed2989ffc226b735355a20d96
#hacker <sk851831@gmail.com>
2015-06-15 19:23:32 +0530

    III - Reborn

--- member/stest.php
+++
@@ -1,6 +0,0 @@
-<?php
-	include('functions_core.php');
-	$new = new create_new_post;
-	$new->getAuth(33);
-
-?>
--- teee.php
+++
@@ -1,7 +0,0 @@
-<?php
-if(strpos("<h1><br></h1>",'</h11>'))
-	echo 'cool';
-else
-	echo 'fool';
-
-?>
